a5d41652cc0fdd7d92627f79f10d413a7bec7870
Addison Emig <addison.emig@mrs-electronics.com>
2026-03-03 15:14:06 -0500

    test: add config auth mode constructor tests

--- test/mrs_sdk_flutter_test.dart
+++ test/mrs_sdk_flutter_test.dart
@@ -3,10 +3,39 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mrs_sdk_flutter/mrs_sdk_flutter.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  group('SpokeZoneConfig', () {
+    test('device mode constructor sets single auth mode', () {
+      final config = SpokeZoneConfig.device(deviceAuth: _deviceCallbacks());
+
+      expect(config.authMode, SpokeZoneAuthMode.device);
+      expect(config.deviceAuth, isNotNull);
+      expect(config.userAuth, isNull);
+      expect(config.baseUri.toString(), 'https://api.spoke.zone');
+    });
+
+    test('user mode constructor sets single auth mode', () {
+      final config = SpokeZoneConfig.user(userAuth: _userCallbacks());
+
+      expect(config.authMode, SpokeZoneAuthMode.user);
+      expect(config.userAuth, isNotNull);
+      expect(config.deviceAuth, isNull);
+      expect(config.baseUri.toString(), 'https://api.spoke.zone');
+    });
   });
+}
+
+DeviceAuthCallbacks _deviceCallbacks() {
+  return DeviceAuthCallbacks(
+    cpuId: () async => 'cpu-1',
+    uuid: () async => 'uuid-1',
+    deviceId: () async => 'device-1',
+    initialDeviceToken: () async => 'initial-device-token',
+  );
+}
+
+UserAuthCallbacks _userCallbacks() {
+  return UserAuthCallbacks(
+    username: () async => 'user-a',
+    password: () async => 'pw-a',
+  );
 }
